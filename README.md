fc78e1965d86190882b869bda049ebe853771e10
haendel7358 <haendel.j@web.de>
2021-08-24 11:18:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-hhy9h3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-hhy9h3)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello" >
+    <h3>{{ msg }}</h3>
+    <br/>
+    <input 
+      type="text"
+      placeholder="Eingabe"
+      ref='eingabe'
+    />
+    <!--Zugriff mit ref auf den DOM  -->
+    <div ref= "elRef">
+      <h3>Zugriff mit ref auf den DOM</h3>
+    </div>
+    <!--  -->
+  </div>
+</template>
+
+<script>
+import {ref, onMounted} from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup(){
+    const eingabe = ref(null)
+    const elRef = ref(null)
+
+    onMounted(()=>{
+      eingabe.value.focus();
+      // Zugriff mit ref auf den DOM
+      console.log(elRef.value.textContent);
+     }
+    )
+   
+    return{
+      eingabe,
+      elRef
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
